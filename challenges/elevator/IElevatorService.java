4e1ce760acc5a706ecb73500cbdc8b8eabb059c0
José Alexandre <59508016+upALX@users.noreply.github.com>
2025-07-22 22:13:30 -0300

    adding interface elavator to service

---
+++ challenges/elevator/IElevatorService.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public interface IElevatorService {
+    List<Integer> leastUsedFloors();
+    List<Character> mostUsedElevators();
+    List<Character> leastUsedElevators();
+    List<Character> busiestShiftsForMostUsedElevators();
+    List<Character> quietestShiftsForLeastUsedElevators();
+    List<Character> busiestShiftsOverall();
+    float usagePercentage(char elevator);
+}
